62cd71ac116e426ea202ea3e7d0daf94683bcb59
Tharindu37 <tharindut520@gmail.com>
2024-07-11 12:41:54 +0530

    plc_barcode_indicator_test_1

---
+++ plc_barcode_indicator_test_1.js
@@ -0,0 +1,125 @@
+const net = require('net');
+const ModbusRTU = require('modbus-serial');
+
+
+const serverPort = 2002;
+const serverAddress = '192.168.1.205'; // Replace with your server's IP address if needed
+let barcode = ''
+function Barcode1Scan(){
+    // Create a TCP client socket
+    const client = new net.Socket();
+
+    // Connect to the server
+    client.connect(serverPort, serverAddress, () => {
+        console.log(`Connected to server at ${serverAddress}:${serverPort}`);
+    });
+
+    // Handle incoming data from the server
+    client.on('data', (data) => {
+    console.log(data)
+        const message = data.toString().trim();
+        if(message!=barcode){
+            readWeight();
+        }
+    });
+
+    // Handle connection close
+    client.on('close', () => {
+        console.log('Connection to server closed');
+        client.removeAllListeners()
+        client.destroy();
+        setTimeout(Barcode1Scan,2000);
+    });
+
+    // Handle connection errors
+    client.on('error', (err) => {
+        console.error('Connection error:', err);
+        client.removeAllListeners()
+        client.destroy();
+        setTimeout(Barcode1Scan,2000);
+    });
+
+    // Handle server connection timeout
+    client.on('timeout', () => {
+        console.error('Connection to server timed out');
+        client.removeAllListeners()
+        client.destroy();
+        setTimeout(Barcode1Scan,2000);
+    });
+
+    // Handle client socket end
+    client.on('end', () => {
+        console.log('Disconnected from server');
+        client.removeAllListeners()
+        client.destroy();
+        setTimeout(Barcode1Scan,2000);
+    });
+}
+
+Barcode1Scan();
+
+function readWeight(){
+    // Create a Modbus client instance
+    const client = new ModbusRTU();
+
+    // Set the properties of the Modbus client
+
+    client.connectTCP('192.168.1.25', { port: 8080 })
+    .then(() => {
+        // Connected
+        console.log('Connected to PLC');
+
+        // Read Holding Registers (example: read from address 0, 1 register)
+        client.readHoldingRegisters(4000, 3)
+        .then(data => {
+            console.log('Read data:', data.data);
+            writeStatus();
+        })
+        .catch(err => {
+            console.error('Error reading:', err);
+        });
+    })
+    .catch(err => {
+        console.error('Connection failed:', err.message);
+    });
+}
+
+function writeStatus(){
+    // Create a Modbus client instance
+    const client = new ModbusRTU();
+
+    // Set the properties of the Modbus client
+    client.connectTCP('192.168.1.221', { port: 502 })
+    .then(() => {
+        // Connected
+        console.log('Connected to PLC');
+
+        // Read Holding Registers (example: read from address 0, 1 register)
+        client.readHoldingRegisters(0, 1)
+        .then(data => {
+            console.log('Read data:', data.data);
+        })
+        .catch(err => {
+            console.error('Error reading:', err);
+        });
+
+        // Write Holding Registers (example: write value 123 to address 10)
+        // client.writeRegisters(10, [123])
+        client.writeRegisters(0, [2])
+        .then(() => {
+            console.log('Write successful');
+        })
+        .catch(err => {
+            console.error('Error writing:', err);
+        })
+        .finally(() => {
+            // Close the connection after reading/writing
+            client.close();
+            console.log('Connection closed');
+        });
+    })
+    .catch(err => {
+        console.error('Connection failed:', err.message);
+    });
+
+}
